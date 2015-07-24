7b2d49605572e527b08dfc9e0c0ccdb2cf4f597b
dans98 <dans1979@gmail.com>
2015-07-23 23:46:32 -0500

    More unit testing changes

--- tests/unit/Exception/ExceptionTest.php
+++ tests/unit/Exception/ExceptionTest.php
@@ -47,14 +47,29 @@ namespace Flair\Exception {
         protected static $code = 999;
 
         /**
+         * holds a previous exception
+         *
+         * @var \Exception
+         */
+        protected static $previous = null;
+
+        /**
          * A simple helper function that returns a thrown exception.
          *
          * @author Daniel Sherman
          */
-        protected function thrower()
+        protected function thrower($multi = false)
         {
+
+            if ($multi) {
+                $grandParent = new Exception('grandParent', 0);
+                $previous = new Exception('parent', 1, $grandParent);
+            } else {
+                $previous = self::$previous;
+            }
+
             try {
-                throw new Exception(self::$msg, self::$code, self::$context);
+                throw new Exception(self::$msg, self::$code, $previous, self::$context);
             } catch (Exception $e) {
                 return $e;
             }
@@ -131,9 +146,26 @@ namespace Flair\Exception {
         public function testToString()
         {
             $e = $this->thrower();
-
             $msg = 'the return value was not what it should be';
             $this->assertTrue(is_string($e->__toString()), $msg);
+        }
+
+        /**
+         * Checks getPrevious  works as expected.
+         *
+         * @author Daniel Sherman
+         * @test
+         * @covers ::getPrevious()
+         */
+        public function testGetPrevious()
+        {
+            $e = $this->thrower();
+            $msg = 'the object is not the correct type';
+            $this->assertNull($e->getPrevious(), $msg);
+
+            $e = $this->thrower(true);
+            $msg = 'the object is not the correct type';
+            $this->assertInstanceOf('Flair\Exception\Exception', $e->getPrevious(), $msg);
         }
 
     }
