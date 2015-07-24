e2e92df54eb6d8d2cf3f2e4df16c39c7fbe4023a
dans98 <dans1979@gmail.com>
2015-07-23 22:11:57 -0500

    Random updates

---
+++ tests/unit/Validation/Core/_TestCallable/TestCallable.php
@@ -0,0 +1,19 @@
+<?php
+class TestCallable
+{
+    /**
+     * a method that always returns true
+     */
+    public function alwaysTrue()
+    {
+        return true;
+    }
+
+    /**
+     * a static method that always returns true
+     */
+    public static function staticAlwaysTrue()
+    {
+        return true;
+    }
+}
