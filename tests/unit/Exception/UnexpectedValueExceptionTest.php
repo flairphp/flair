3eedd123c657c8fa12f6e9cc5ce79d6260e4bf74
dans98 <dans1979@gmail.com>
2015-08-20 15:09:45 -0500

    More testing stuff

---
+++ tests/unit/Exception/UnexpectedValueExceptionTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Flair\Exception {
+    /**
+     * The Unit test for the UnexpectedValueException class.
+     *
+     * @author Daniel Sherman
+     * @coversDefaultClass \Flair\Exception\UnexpectedValueException
+     */
+    class UnexpectedValueExceptionTest extends ExceptionTest
+    {
+        /**
+         * holds the class name being tested
+         *
+         * @var string
+         */
+        protected static $class = 'Flair\Exception\UnexpectedValueException';
+    }
+}
