8b67e3a3265e6f0759a7cad2344b27f006f447e3
dans98 <dans1979@gmail.com>
2015-09-11 16:30:02 -0500

    More validation work

---
+++ src/Flair/Validation/ExceptionInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace Flair\Validation {
+    /**
+     * Allows hinting for any exception in the
+     * Flair\Validation namespace.
+     *
+     * @author Daniel Sherman
+     */
+    interface ExceptionInterface
+    {}
+}
