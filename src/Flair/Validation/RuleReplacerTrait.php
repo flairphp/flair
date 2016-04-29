88ca9bbfc9eef27c6f186769e1d43482d93fd133
dans98 <dans1979@gmail.com>
2016-04-28 22:53:59 -0500

    Fixing a testing strict bug

--- src/Flair/Validation/RuleReplacerTrait.php
+++ src/Flair/Validation/RuleReplacerTrait.php
@@ -1,50 +1,54 @@
 <?php
 namespace Flair\Validation {
 
-	/**
-	 * Provides The minimal functionality needed to manipulate a replacer.
-	 *
-	 * @author Daniel Sherman
-	 */
-	trait RuleReplacerTrait {
+    /**
+     * Provides The minimal functionality needed to manipulate a replacer.
+     *
+     * @author Daniel Sherman
+     */
+    trait RuleReplacerTrait
+    {
 
-		/**
-		 * The replacer associated with the object.
-		 *
-		 * @var Replacer
-		 */
-		protected $replacer = null;
+        /**
+         * The replacer associated with the object.
+         *
+         * @var Replacer
+         */
+        protected $replacer = null;
 
-		/**
-		 * Returns a reference to the internal replacer object.
-		 *
-		 * @return Replacer A replacer object, or null if no replacer has been set
-		 */
-		public function getReplacer() {
-			return $this->replacer;
-		}
+        /**
+         * Returns a reference to the internal replacer object.
+         *
+         * @return Replacer A replacer object, or null if no replacer has been set
+         */
+        public function getReplacer()
+        {
+            return $this->replacer;
+        }
 
-		/**
-		 * Sets the internal replacer object.
-		 *
-		 * @param Replacer $replacer The value to assign.
-		 * @throws InvalidArgumentException If $replacer isn't.
-		 */
-		public function setReplacer(ReplacerInterface $replacer) {
-			$this->replacer = $replacer;
-		}
+        /**
+         * Sets the internal replacer object.
+         *
+         * @param Replacer $replacer The value to assign.
+         * @throws InvalidArgumentException If $replacer isn't.
+         */
+        public function setReplacer(ReplacerInterface $replacer)
+        {
+            $this->replacer = $replacer;
+        }
 
-		/**
-		 * Indicates if a replacer is currently set.
-		 *
-		 * @return bool
-		 */
-		public function hasReplacer() {
-			if ($this->replacer !== null) {
-				return true;
-			} else {
-				return false;
-			}
-		}
-	}
+        /**
+         * Indicates if a replacer is currently set.
+         *
+         * @return bool
+         */
+        public function hasReplacer()
+        {
+            if ($this->replacer !== null) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 }
